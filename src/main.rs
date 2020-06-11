ffd4a21d438f39afb1c57e9fb8968c618dd47353
ay65535 <ay65535@icloud.com>
2020-06-11 14:54:13 +0900

    Chap2 finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // _ は、包括値
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!"); //やったね！
+                break;
+            }
+        }
+    }
 }
